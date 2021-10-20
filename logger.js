f93f2fd52ff1bab42d79a5f443900f398bc92f0c
EveryOtherUsernameWasTaken <92779321+EveryOtherUsernameWasTaken@users.noreply.github.com>
2021-10-21 00:11:51 +0700

    ok

---
+++ logger.js
@@ -0,0 +1,12 @@
+const EventEmitter = require('events');
+
+var url = 'http://mylogger.io/log';
+
+class Logger extends EventEmitter {
+    log(message) {
+        console.log(message);
+        this.emit('message logged', {id: 1, url: 'http://'} );
+    }
+}
+
+module.export = Logger;
